01d9b95d81a1f8e431db5a8cf993e4fe02c4a3a8
Achim D. Brucker <adbrucker@0x5f.org>
2016-09-21 09:12:54 +0100

    Initial boilerplate.

--- crx-tool.py
+++ crx-tool.py
@@ -15,3 +15,37 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #
+
+import argparse
+
+# https://developer.chrome.com/extensions/crx
+    
+def verify_crxfile (verbose, filename):
+    print("Not yet implemented: verify crxfile(", verbose,", ", filename, ")")
+
+def extract_crxfile(verbose, force, filename):
+    print("Not yet implemented: extract crxfile(",verbose,", ", force,", ", filename, ")")
+
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("file", help="chrome extension archive (*.crx)")
+    parser.add_argument("-c", "--check", help="verify format and signature of <file>",
+                        action="store_true")
+    parser.add_argument("-e", "--extract", help="extract <file>",
+                        action="store_true")
+    parser.add_argument("-f", "--force", help="apply action also to (potential) invalid files",
+                        action="store_true")
+    parser.add_argument("-v", "--verbose", help="increase verbosity",
+                        action="store_true")
+    args = parser.parse_args()
+    
+    if args.extract:
+        retval = extract_crxfile(args.verbose, args.force, args.file)
+    else: 
+        retval = verify_crxfile(args.verbose, args.file)
+
+    exit(retval)    
+    
+if __name__ == "__main__":
+    main()
