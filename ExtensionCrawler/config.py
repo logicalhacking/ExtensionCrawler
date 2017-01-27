bd53b117e5a972a1aa5525552662db37e90f5d67
Achim D. Brucker <adbrucker@0x5f.org>
2017-01-27 22:40:07 +0000

    Refactoring.

--- ExtensionCrawler/config.py
+++ ExtensionCrawler/config.py
@@ -16,8 +16,61 @@
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #
 
+
 def const_sitemap_url():
     return "https://chrome.google.com/webstore/sitemap"
 
+
 def const_sitemap_scheme():
     return "http://www.sitemaps.org/schemas/sitemap/0.9"
+
+
+def const_overview_url(id):
+    return 'https://chrome.google.com/webstore/detail/{}'.format(id)
+
+
+def const_store_url():
+    return 'https://chrome.google.com/webstore'
+
+
+def const_review_url():
+    return 'https://chrome.google.com/reviews/components'
+
+
+def const_support_url():
+    return 'https://chrome.google.com/reviews/components'
+
+
+def const_download_url():
+    return 'https://clients2.google.com/service/update2/crx?response=redirect&nacl_arch=x86-64&prodversion=9999.0.9999.0&x=id%3D{}%26uc'
+
+
+def const_categories():
+    return [
+        'extensions', 'ext/22-accessibility', 'ext/10-blogging',
+        'ext/15-by-google', 'ext/11-web-development', 'ext/14-fun',
+        'ext/6-news', 'ext/28-photos', 'ext/7-productivity',
+        'ext/38-search-tools', 'ext/12-shopping', 'ext/1-communication',
+        'ext/13-sports'
+    ]
+
+
+def const_support_payload(ext_id, start, end):
+    return (
+        'req={{ "appId":94,' + '"version":"150922",' + '"hl":"en",' +
+        '"specs":[{{"type":"CommentThread",' +
+        '"url":"http%3A%2F%2Fchrome.google.com%2Fextensions%2Fpermalink%3Fid%3D{}",'
+        + '"groups":"chrome_webstore_support",' + '"startindex":"{}",' +
+        '"numresults":"{}",' + '"id":"379"}}],' + '"internedKeys":[],' +
+        '"internedValues":[]}}').format(ext_id, start, end)
+
+
+def const_review_payload(ext_id, start, end):
+    return (
+        'req={{ "appId":94,' + '"version":"150922",' + '"hl":"en",' +
+        '"specs":[{{"type":"CommentThread",' +
+        '"url":"http%3A%2F%2Fchrome.google.com%2Fextensions%2Fpermalink%3Fid%3D{}",'
+        + '"groups":"chrome_webstore",' + '"sortby":"cws_qscore",' +
+        '"startindex":"{}",' + '"numresults":"{}",' + '"id":"428"}}],' +
+        '"internedKeys":[],' + '"internedValues":[]}}').format(ext_id, start,
+                                                               end)
