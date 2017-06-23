b080543e9bd16d680b48f3113f8812b53f434ece
Michael Herzberg <mail@michael-herzberg.de>
2017-06-23 14:44:07 +0100

    Added script to generate small db.

---
+++ scripts/generate_small_db.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/bash
+
+# Usage:
+# ./generate_small_db.sh [BASEDIR] [DBPATH] [EXTENSIONCRAWLER]
+#   [BASEDIR]           path to extension data, needs to contain aaa, aab, etc (defaults to sharc path)
+#   [DBPATH]            path to output db (defaults to ~/aa-ac.sqlite)
+#   [EXTENSIONCRAWLER]  path to git repo (defaults to ~/ExtensionCrawler)
+
+BASEDIR=${1:-/shared/brucker_research1/Shared/BrowserExtensions/data}
+DBPATH=${2:-~/aa-ac.sqlite}
+EXTENSIONCRAWLER=${3:-~/ExtensionCrawler}
+
+FIRSTDB=$(find "$BASEDIR"/aa* -name "*.sqlite" | head -n 1)
+sqlite3 "$FIRSTDB" .schema | sqlite3 "$DBPATH"
+echo "Used $FIRSTDB for schema"
+
+find "$BASEDIR"/aa* -name "*.sqlite" -exec "$EXTENSIONCRAWLER/scripts/merge_dbs.sh" "{}" "$DBPATH" \;
+find "$BASEDIR"/ab* -name "*.sqlite" -exec "$EXTENSIONCRAWLER/scripts/merge_dbs.sh" "{}" "$DBPATH" \;
+find "$BASEDIR"/ac* -name "*.sqlite" -exec "$EXTENSIONCRAWLER/scripts/merge_dbs.sh" "{}" "$DBPATH" \;
