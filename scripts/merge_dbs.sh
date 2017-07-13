a901fa30bbea13af15ffeb5ee5fa00d0d8453dfd
Michael Herzberg <mail@michael-herzberg.de>
2017-07-13 14:43:37 +0100

    Adjusted merge scripts for fts tables.

--- scripts/merge_dbs.sh
+++ scripts/merge_dbs.sh
@@ -7,11 +7,30 @@ if [ -z $FROM_DB ] || ! [ -f $FROM_DB ]; then
   exit 1
 fi
 
-if [ -z $TO_DB ] || ! [ -f $TO_DB ]; then
-  echo "destination db not provided or does not exist"
+if [ -z $TO_DB ]; then
+  echo "destination db not provided"
   exit 1
+fi
+
+if ! [ -f $TO_DB ]; then
+  echo "Creating $TO_DB ..."
+  sqlite3 $FROM_DB .schema | grep -Eiv \
+    -e "^CREATE TABLE IF NOT EXISTS (\"|')?[a-z]+_content(\"|')?\(" \
+    -e "^CREATE TABLE IF NOT EXISTS (\"|')?[a-z]+_docsize(\"|')?\(" \
+    -e "^CREATE TABLE IF NOT EXISTS (\"|')?[a-z]+_segments(\"|')?\(" \
+    -e "^CREATE TABLE IF NOT EXISTS (\"|')?[a-z]+_stat(\"|')?\(" \
+    -e "^CREATE TABLE IF NOT EXISTS (\"|')?[a-z]+_segdir(\"|')?\(" \
+  | sqlite3 $TO_DB
 fi
 
 echo "Merging $FROM_DB into $TO_DB..."
 
-sqlite3 $FROM_DB .dump | grep -v "^CREATE TABLE" | sqlite3 $TO_DB
+sqlite3 $FROM_DB .dump | grep -Eiv \
+  -e "^CREATE TABLE" \
+  -e "^INSERT INTO (\"|')?sqlite_master(\"|')?" \
+  -e "^INSERT INTO (\"|')?[a-z]+_segments(\"|')? " \
+  -e "^INSERT INTO (\"|')?[a-z]+_segdir(\"|')? " \
+  -e "^INSERT INTO (\"|')?[a-z]+_docsize(\"|')? " \
+  -e "^INSERT INTO (\"|')?[a-z]+_stat(\"|')? " \
+| sed -r "s/^INSERT INTO ([a-z]+)_content VALUES\([[:digit:]]+,/INSERT INTO \1 VALUES(/I" \
+| sqlite3 $TO_DB
