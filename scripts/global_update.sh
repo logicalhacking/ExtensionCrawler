d6d1d21d056085fbb157c5947d683f1e52bd4373
Achim D. Brucker <adbrucker@0x5f.org>
2017-07-24 10:26:03 +0100

    Bug fix: Append log to log instead of overwritting it.

--- scripts/global_update.sh
+++ scripts/global_update.sh
@@ -19,49 +19,49 @@ SQLITE=`which sqlite3`
 # Update extensions
 (cd $CRAWLERHOME; (./crawler -d -a $ARCHIVE > $LOGPREFIX.log))
 
-date +"* Start Creating aa-ac.sqlite Data Base (%c) using $SQLITE" | tee $LOG
+date +"* Start Creating aa-ac.sqlite Data Base (%c) using $SQLITE" | tee -a $LOG
 # Update small database
 rm -f $ARCHIVE/db/aa-ac.sqlite
 (cd $CRAWLERHOME; (./scripts/generate_small_db.sh $ARCHIVE/data $ARCHIVE/db/aa-ac.sqlite &> $LOGPREFIX-sqlite-aa-ac.log))
 if [ $? -ne "0" ]; then 
-  echo "    Creation of aa-ac.sqlite failed - see log file for details" | tee $LOG
+  echo "    Creation of aa-ac.sqlite failed - see log file for details" | tee -a $LOG
 else 
   SIZE=`du -k $ARCHIVE/db/aa-ac.sqlite | cut -f1`
-  echo "    Created aa-ac.sqlite successfully ($SIZE kb)" | tee $LOG
+  echo "    Created aa-ac.sqlite successfully ($SIZE kb)" | tee -a $LOG
 fi
 
 if [ -f "$ARCHIVE"/db/aa-ac.sqlite ]; then 
-  date +'* Start Compressing aa-ac.sqlite Data Base (%c)' | tee $LOG
+  date +'* Start Compressing aa-ac.sqlite Data Base (%c)' | tee -a $LOG
   rm -f $ARCHIVE/db/aa-ac.sqlite.bz2
   pbzip2 -f "$ARCHIVE"/db/aa-ac.sqlite 
   if [ $? -ne "0" ]; then 
-    echo "    Creation of aa-ac.sqlite.bz2 failed"  | tee $LOG
+    echo "    Creation of aa-ac.sqlite.bz2 failed"  | tee -a $LOG
   else 
     SIZE=`du -k $ARCHIVE/db/aa-ac.sqlite.bz2 | cut -f1`
-    echo "    Created aa-ac.sqlite.bz2 successfully ($SIZE kb)" | tee $LOG
+    echo "    Created aa-ac.sqlite.bz2 successfully ($SIZE kb)" | tee -a $LOG
   fi
 fi
 
-date +"* Start Creating full.sqlite Data Base (%c) using $SQLITE" | tee $LOG
+date +"* Start Creating full.sqlite Data Base (%c) using $SQLITE" | tee -a $LOG
 # Update full database
 rm -f $ARCHIVE/db/full.sqlite
 find "$ARCHIVE"/data/ -name "*.sqlite" -exec "$CRAWLERHOME/scripts/merge_dbs.sh" "{}" "$ARCHIVE"/db/full.sqlite \; &> $LOGPREFIX-sqlite-full.log
 if [ $? -ne "0" ]; then 
-  echo "    Creation of full.sqlite failed - see log file for details" | tee $LOG
+  echo "    Creation of full.sqlite failed - see log file for details" | tee -a $LOG
 else 
   SIZE=`du -k $ARCHIVE/db/full.sqlite | cut -f1`
-  echo "    Created full.sqlite successfully ($SIZE kb)" | tee $LOG
+  echo "    Created full.sqlite successfully ($SIZE kb)" | tee -a $LOG
 fi
 
 if [ -f "$ARCHIVE"/db/full.sqlite ]; then 
-  date +'* Start Compressing full.sqlite Data Base (%c)' | tee $LOG
+  date +'* Start Compressing full.sqlite Data Base (%c)' | tee -a $LOG
   rm -f $ARCHIVE/db/full.sqlite.bz2
   pbzip2 -f "$ARCHIVE"/db/full.sqlite 
   if [ $? -ne "0" ]; then 
-    echo "    Creation of full.sqlite.bz2 failed" | tee $LOG
+    echo "    Creation of full.sqlite.bz2 failed" | tee -a $LOG
   else 
     SIZE=`du -k $ARCHIVE/db/full.sqlite.bz2 | cut -f1`
-    echo "    Created full.sqlite.bz2 successfully ($SIZE kb)" | tee $LOG
+    echo "    Created full.sqlite.bz2 successfully ($SIZE kb)" | tee -a $LOG
   fi
 fi
 
@@ -70,5 +70,5 @@ fi
 #   pbzip2 -f $f &> /dev/null 
 # done 
 
-date +'* Update Finished (%c)' | tee $LOG
+date +'* Update Finished (%c)' | tee -a $LOG
 
