d05ca9678e3e305e63d2d0cf67b61fa7c20b0e95
Achim D. Brucker <adbrucker@0x5f.org>
2017-07-29 10:57:35 +0100

    Refactoring.

--- ExtensionCrawler/config.py
+++ ExtensionCrawler/config.py
@@ -13,46 +13,57 @@
 # GNU General Public License for more details.
 #
 # You should have received a copy of the GNU General Public License
-# along with this program.  If not, see <http://www.gnu.org/licenses/>.
-#
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+"""Global configuration of the Extension Crawler and related tools."""
 
 import os
 import json
 
 
 def const_sitemap_url():
+    """Sitemap URL."""
     return "https://chrome.google.com/webstore/sitemap"
 
 
 def const_sitemap_scheme():
+    """URL of Sitemap schema."""
     return "http://www.sitemaps.org/schemas/sitemap/0.9"
 
 
-def const_overview_url(id):
-    return 'https://chrome.google.com/webstore/detail/{}'.format(id)
+def const_overview_url(ext_id):
+    """URL template for the overview page of an extension."""
+    return 'https://chrome.google.com/webstore/detail/{}'.format(ext_id)
 
 
 def const_store_url():
+    """Main URL of the Chrome store."""
     return 'https://chrome.google.com/webstore'
 
 
 def const_review_url():
+    """Base URL of the review page of an extension."""
     return 'https://chrome.google.com/reviews/components'
 
 
 def const_review_search_url():
+    """Base URL for review search."""
     return 'https://chrome.google.com/reviews/json/search'
 
 
 def const_support_url():
+    """Base URL for support pages."""
     return 'https://chrome.google.com/reviews/components'
 
 
 def const_download_url():
-    return 'https://clients2.google.com/service/update2/crx?response=redirect&nacl_arch=x86-64&prodversion=9999.0.9999.0&x=id%3D{}%26uc'
+    """Base download URL."""
+    return ('https://clients2.google.com/service/update2/' +
+            'crx?response=redirect&nacl_arch=x86-64&' +
+            'prodversion=9999.0.9999.0&x=id%3D{}%26uc')
 
 
 def const_categories():
+    """List of known categories."""
     return [
         'extensions', 'ext/22-accessibility', 'ext/10-blogging',
         'ext/15-by-google', 'ext/11-web-development', 'ext/14-fun',
@@ -63,6 +74,7 @@ def const_categories():
 
 
 def const_support_payload(ext_id, start, end):
+    """Payload for requesting support pages."""
     return (
         'req={{ "appId":94,' + '"version":"150922",' + '"hl":"en",' +
         '"specs":[{{"type":"CommentThread",' +
@@ -73,17 +85,19 @@ def const_support_payload(ext_id, start, end):
 
 
 def const_review_payload(ext_id, start, end):
+    """Payload for requesting review pages."""
     return (
         'req={{ "appId":94,' + '"version":"150922",' + '"hl":"en",' +
         '"specs":[{{"type":"CommentThread",' +
         '"url":"http%3A%2F%2Fchrome.google.com%2Fextensions%2Fpermalink%3Fid%3D{}",'
         + '"groups":"chrome_webstore",' + '"sortby":"cws_qscore",' +
         '"startindex":"{}",' + '"numresults":"{}",' + '"id":"428"}}],' +
-        '"internedKeys":[],' + '"internedValues":[]}}').format(
-            ext_id, start, end)
+        '"internedKeys":[],' + '"internedValues":[]}}').format(ext_id, start,
+                                                               end)
 
 
 def const_review_search_payload(params):
+    """Payload for searches."""
     pre = """req={"applicationId":94,"searchSpecs":["""
     post = """]}&requestSource=widget"""
     args = []
@@ -100,19 +114,23 @@ def const_review_search_payload(params):
     return pre + ",".join(args) + post
 
 
-def get_local_archive_dir(id):
-    return "{}".format(id[:3])
+def get_local_archive_dir(ext_id):
+    """Local archive dir of extension."""
+    return "{}".format(ext_id[:3])
 
 
 def archive_file(archivedir, ext_id):
+    """Archive tar of an extension."""
     return os.path.join(
         str(archivedir), get_local_archive_dir(ext_id), ext_id + ".tar")
 
 
 def db_file(archivedir, ext_id):
+    """DB (sqlite) file of an extension."""
     return os.path.join(archivedir,
                         get_local_archive_dir(ext_id), ext_id + ".sqlite")
 
+
 def jsloc_timeout():
-    # Maximum number of seconds for counting jsloc per extension
+    """Maximum number of seconds for counting jsloc per extension."""
     return 600
