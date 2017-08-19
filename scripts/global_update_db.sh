1bd097b863094665445d820a03aa72f2f65a76a6
Achim D. Brucker <adbrucker@0x5f.org>
2017-08-19 12:29:18 +0100

    Force /usr/bin/mail as mailer.

--- scripts/global_update_db.sh
+++ scripts/global_update_db.sh
@@ -61,7 +61,7 @@ if [ -f "$ARCHIVE"/db/full.build.sqlite ]; then
   queries=`dirname $0`
   result=`sqlite3 "$ARCHIVE"/db/full.sqlite < $queries/../queries/get_added_permissions.sql`
   echo $result > $LOGPREFIX-analysis.log
-  echo $result | mail root -s "Extension Analysis" 
+  echo $result | /usr/bin/mail -s "Extension Analysis" root
   date +'*       Analysis finished (%c)' | tee -a $LOG
   
   date +'* Start Compressing full.build.sqlite Data Base (%c)' | tee -a $LOG
