1a09277e70ec130333dcb156294123799f84fffa
Achim D. Brucker <adbrucker@0x5f.org>
2017-08-28 14:42:16 +0100

    Added mysqlclient to dependencies.

--- setup.py
+++ setup.py
@@ -5,5 +5,5 @@ setup(
     description='A collection of utilities for downloading and analyzing browser extension from the Chrome Web store.',
     author='Achim D. Brucker, Michael Herzberg',
     license='GPL 3.0',
-    install_requires=['requests', 'pycrypto', 'beautifulsoup4', 'python_dateutil']
+    install_requires=['requests', 'pycrypto', 'beautifulsoup4', 'python_dateutil', 'mysqlclient']
 )
