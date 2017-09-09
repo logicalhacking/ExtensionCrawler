c3053427c0f482c40b34301a8dbc9571825242e8
Achim D. Brucker <adbrucker@0x5f.org>
2017-09-09 23:13:26 +0100

    Added method for obtaining initial commit date and pulling git repos.

---
+++ ExtensionCrawler/cdnjs-git.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python3.5
+#
+# Copyright (C) 2016,2017 The University of Sheffield, UK
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+""" Module for obtaining md5/sha1/sha256 hashes for all files available
+    at CDNJS.com by mining the cdnjs git repository."""
+
+import git
+import dateutil.parser
+
+
+def get_add_date(gitobj, filename):
+    """Method for getting the initial add/commit date of a file."""
+    try:
+        add_date_string = gitobj.log("--follow", "--format=%aD", "--reverse",
+                                     filename).splitlines()[0]
+        return dateutil.parser.parse(add_date_string)
+    except Exception:
+        return None
+
+
+def pull(gitrepo):
+    """Pull new updates from remote origin."""
+    try:
+        cdnjs_origin = gitrepo.remotes.origin
+        return cdnjs_origin.pull()
+    except Exception:
+        return None
