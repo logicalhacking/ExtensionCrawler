9031a5cbb562aa80f5b152d54c53aaacc3b05db3
Achim D. Brucker <adbrucker@0x5f.org>
2017-10-17 06:50:48 +0100

    Added log file.

--- scripts/update_cdnjs.sh
+++ scripts/update_cdnjs.sh
@@ -1,14 +1,17 @@
 #!/bin/bash
 
+LOGPREFIX=$ARCHIVE/log/`date --iso-8601=ns`
+LOG=$LOGPREFIX-cdnjs.log 
+
 SING_IMG=/shared/brucker_research1/Shared/BrowserExtensions/archive/filedb/ExtensionCrawler-cdnjs.img
 SING_EXEC="singularity exec -w --pwd /opt/ExtensionCrawler -B $TMPDIR:/tmp $SING_IMG"
 ls "$SING_IMG" > /dev/null
 
 # Update production branch of WebCrawler repository
-$SING_EXEC git fetch
-$SING_EXEC git checkout production
-$SING_EXEC git pull
+$SING_EXEC git fetch > $LOGPREFIX.log
+$SING_EXEC git checkout production >> $LOGPREFIX.log
+$SING_EXEC git pull >> $LOGPREFIX.log
 
 # Update cdnjs git repository and update cdnjs data base table
-$SING_EXEC ./cdnjs-git-miner -v -p 1 -u -a /opt/archive
+$SING_EXEC ./cdnjs-git-miner -v -p 1 -u -a /opt/archive >> $LOGPREFIX.log
 
