85b7f0175160e446640c49ca8695ad348e8cc163
Achim D. Brucker <adbrucker@0x5f.org>
2017-10-17 18:14:33 +0100

    Bug fix: name of log file.

--- scripts/update_cdnjs.sh
+++ scripts/update_cdnjs.sh
@@ -10,10 +10,10 @@ SING_EXEC="singularity exec -w --pwd /opt/ExtensionCrawler -B $TMPDIR:/tmp $SING
 ls "$SING_IMG" > /dev/null
 
 # Update production branch of WebCrawler repository
-$SING_EXEC git fetch > $LOGPREFIX.log
-$SING_EXEC git checkout production >> $LOGPREFIX.log
-$SING_EXEC git pull >> $LOGPREFIX.log
+$SING_EXEC git fetch > $LOG.log
+$SING_EXEC git checkout production >> $LOG.log
+$SING_EXEC git pull >> $LOG.log
 
 # Update cdnjs git repository and update cdnjs data base table
-$SING_EXEC ./cdnjs-git-miner -v -p 1 -u -a /opt/archive >> $LOGPREFIX.log
+$SING_EXEC ./cdnjs-git-miner -v -p 1 -u -a /opt/archive >> $LOG.log
 
