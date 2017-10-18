6851223a6dbb781e27e7b89fecd144285637d615
Achim D. Brucker <adbrucker@0x5f.org>
2017-10-18 08:35:04 +0100

    Fixed log file name.

--- scripts/update_cdnjs.sh
+++ scripts/update_cdnjs.sh
@@ -10,10 +10,10 @@ SING_EXEC="singularity exec -w --pwd /opt/ExtensionCrawler -B $TMPDIR:/tmp $SING
 ls "$SING_IMG" > /dev/null
 
 # Update production branch of WebCrawler repository
-$SING_EXEC git fetch > $LOG.log
-$SING_EXEC git checkout production >> $LOG.log
-$SING_EXEC git pull >> $LOG.log
+$SING_EXEC git fetch > $LOG
+$SING_EXEC git checkout production >> $LOG
+$SING_EXEC git pull >> $LOG
 
 # Update cdnjs git repository and update cdnjs data base table
-$SING_EXEC ./cdnjs-git-miner -v -p 1 -u -a /opt/archive >> $LOG.log
+$SING_EXEC ./cdnjs-git-miner -v -p 1 -u -a /opt/archive >> $LOG
 
