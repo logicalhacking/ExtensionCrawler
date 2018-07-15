595f0f8759178d7d6535e354757ce858e2abe751
Michael Herzberg <michael@mherzberg.de>
2018-07-15 19:19:26 +0100

    Use 16 threads to discover new extensions.

--- ExtensionCrawler/discover.py
+++ ExtensionCrawler/discover.py
@@ -20,38 +20,61 @@
 from xml.etree.ElementTree import fromstring
 import re
 import requests
+from pebble import ThreadPool
 from ExtensionCrawler import config
 
 
-def crawl_nearly_all_of_ext_ids():
+def get_inner_elems(doc):
+    """Get inner element."""
+    return fromstring(doc).iterfind(r".//{{{}}}loc".format(
+        config.const_sitemap_scheme()))
+
+
+def is_generic_url(url):
+    """Check if URL is a generic extension URL."""
+    """The urls with a language parameter attached return a subset"""
+    """of the ids that get returned by the plain urls, therefore we"""
+    """skip urls with a language parameter."""
+
+    return re.match(r"^{}\?shard=\d+&numshards=\d+$".format(
+        config.const_sitemap_url()), url)
+
+
+def iterate_shard(shard_url):
+    if is_generic_url(shard_url):
+        shard = requests.get(shard_url, timeout=10).text
+        for inner_elem in get_inner_elems(shard):
+            overview_url = inner_elem.text
+            yield re.search("[a-z]{32}", overview_url).group(0)
+
+
+def process_shard(shard_url):
+    return list(iterate_shard(shard_url))
+
+
+def crawl_nearly_all_of_ext_ids(max_ids=None):
     """Crawl extension ids available in Chrome store."""
 
-    def get_inner_elems(doc):
-        """Get inner element."""
-        return fromstring(doc).iterfind(r".//{{{}}}loc".format(
-            config.const_sitemap_scheme()))
+    shard_urls = [shard_elem.text for shard_elem in get_inner_elems(
+            requests.get(config.const_sitemap_url(), timeout=10).text)]
+    with ThreadPool(16) as pool:
+        future = pool.map(process_shard, shard_urls, chunksize=1)
+        iterator = future.result()
 
-    def is_generic_url(url):
-        """Check if URL is a generic extension URL."""
-        """The urls with a language parameter attached return a subset"""
-        """of the ids that get returned by the plain urls, therefore we"""
-        """skip urls with a language parameter."""
+        returned_ids = 0
+        while True:
+            try:
+                for extid in next(iterator):
+                    yield extid
+                    returned_ids += 1
+                    if max_ids is not None and returned_ids >= max_ids:
+                        pool.stop()
+                        return
+            except StopIteration:
+                return
 
-        return re.match(r"^{}\?shard=\d+&numshards=\d+$".format(
-            config.const_sitemap_url()), url)
-
-    for shard_elem in get_inner_elems(
-            requests.get(config.const_sitemap_url(), timeout=10).text):
-        shard_url = shard_elem.text
-        if is_generic_url(shard_url):
-            shard = requests.get(shard_url, timeout=10).text
-            for inner_elem in get_inner_elems(shard):
-                overview_url = inner_elem.text
-                yield re.search("[a-z]{32}", overview_url).group(0)
-
-
-def get_new_ids(known_ids):
+def get_new_ids(known_ids, max_ids=None):
     """Discover new extension ids."""
-    for discovered_id in crawl_nearly_all_of_ext_ids():
+    for discovered_id in crawl_nearly_all_of_ext_ids(max_ids):
         if discovered_id not in known_ids:
             yield discovered_id
