4b5cc276ee3377f15f34c05f77f389b82991ce4f
Michael Herzberg <michael@mherzberg.de>
2018-07-16 19:14:24 +0100

    Added option to use INSERT DELAYED with create-db.

--- ExtensionCrawler/dbbackend/mysql_backend.py
+++ ExtensionCrawler/dbbackend/mysql_backend.py
@@ -29,9 +29,10 @@ from ExtensionCrawler.util import log_info, log_error, log_warning
 
 
 class MysqlBackend:
-    def __init__(self, ext_id, try_wait=config.const_mysql_try_wait(), maxtries=config.const_mysql_maxtries(),
+    def __init__(self, ext_id, delayed=False, try_wait=config.const_mysql_try_wait(), maxtries=config.const_mysql_maxtries(),
                  **kwargs):
         self.ext_id = ext_id
+        self.delayed = delayed
         self.dbargs = kwargs
         self.try_wait = try_wait
         self.maxtries = maxtries
@@ -56,29 +57,32 @@ class MysqlBackend:
             sorted_arglist = self.sort_by_primary_key(table, arglist)
             args = [tuple(arg.values()) for arg in sorted_arglist]
 
-            # Looks like this, for example:
-            # INSERT INTO category VALUES(extid,date,category) (%s,%s,%s)
-            #   ON DUPLICATE KEY UPDATE extid=VALUES(extid),date=VALUES(date)
-            #   ,category=VALUES(category)
-            query = "INSERT INTO {}({}) VALUES ({}) ON DUPLICATE KEY UPDATE {}".format(
-                table,
-                ",".join(sorted_arglist[0].keys()),
-                ",".join(len(args[0]) * ["%s"]),
-                ",".join(
-                    ["{c}=VALUES({c})".format(c=c) for c in sorted_arglist[0].keys()]))
+            if self.delayed:
+                query = "INSERT DELAYED INTO {}({}) VALUES ({})".format(
+                    table,
+                    ",".join(sorted_arglist[0].keys()),
+                    ",".join(len(args[0]) * ["%s"]))
+            else:
+                # Looks like this, for example:
+                # INSERT INTO category VALUES(extid,date,category) (%s,%s,%s)
+                #   ON DUPLICATE KEY UPDATE extid=VALUES(extid),date=VALUES(date)
+                #   ,category=VALUES(category)
+                query = "INSERT INTO {}({}) VALUES ({}) ON DUPLICATE KEY UPDATE {}".format(
+                    table,
+                    ",".join(sorted_arglist[0].keys()),
+                    ",".join(len(args[0]) * ["%s"]),
+                    ",".join(
+                        ["{c}=VALUES({c})".format(c=c) for c in sorted_arglist[0].keys()]))
             start = time.time()
             self.retry(lambda: self.cursor.executemany(query, args))
             log_info("* Inserted {} bytes into {}, taking {:.2f}s.".format(sum([sys.getsizeof(arg) for arg in args]),
                                                                            table, time.time() - start), 3)
-        start = time.time()
-        self.db.commit()
-        log_info("* DB commit took {:.2f}s".format(time.time() - start), 2)
 
     def _create_conn(self):
         if self.db is None:
             log_info("* self.db is None,  open new connection ...", 3)
             self.db = MySQLdb.connect(**self.dbargs)
-            self.db.autocommit(False)
+            self.db.autocommit(True)
             log_info("* success", 4)
         if self.cursor is None:
             log_info("* self.cursor is None,  assigning new cursor ...", 3)
